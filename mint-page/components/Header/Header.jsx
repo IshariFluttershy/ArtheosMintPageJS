ed79768f5a2f7b84733b883934804280adc18905
Gally <tanguy.morvant2@gmail.com>
2023-01-19 00:35:44 +0100

    React app deleted Next app created Basic front end architecture done

---
+++ mint-page/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import {Flex, Text} from "@chakra-ui/react";
+
+const Header = () => {
+    return (
+        <Flex >
+            Header
+        </Flex>
+    )
+}
+
+export default Header;
---
+++ mint-page/pages/index.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import { Flex } from "@chakra-ui/react";
+import Layout from "../components/Layout/Layout.jsx"
+
+export default function Home() {
+  return (
+    <Layout>
+      <Flex align="center" justify="center">
+        Contenu
+      </Flex>
+    </Layout>
+  )
+}
