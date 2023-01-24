12827599c0ba000642d023b75bc68af3754c9617
Gally <tanguy.morvant2@gmail.com>
2023-01-24 13:18:49 +0100

    Added future smart contract data retreiver

--- mint-page/pages/index.js
+++ mint-page/pages/index.js
@@ -1,19 +1,86 @@
 import React, {useState, useEffect} from "react";
 import { Flex, Button, Text, useToast, Spinner } from "@chakra-ui/react";
 import Layout from "../components/Layout/Layout"
+import useEthersProvider from "../hooks/useEthersProvider";
 import Before from "../components/Before/Before"
 import Reveal from "../components/Reveal/Reveal"
 import WhitelistSale from "../components/WhitelistSale/WhitelistSale"
 import PublicSale from "../components/PublicSale/PublicSale"
 import SoldOut from "../components/SoldOut/SoldOut"
+import { ethers } from "ethers";
 
 
 
 export default function Home() {
+
+  const { account, provider } = useEthersProvider();
+  const [isLoading, setIsLoading] = useState(false);
+  const [sellingStep, setSellingStep] = useState(null);
+  const [saleStartTime, setSaleStartTime] = useState(null);
+  const [BNWlSalePrice, setBNWlSalePrice] = useState(null);
+  const [wlSalePrice, setWlSalePrice] = useState(null);
+  const [BNPublicSalePrice, setBNPublicSalePrice] = useState(null);
+  const [publicSalePrice, setPublicSalePrice] = useState(null);
+  const [totalSupply, setTotalSupply] = useState(null);
+
+  const toast = useToast();
+  const contractAddress = "";
+
+  useEffect(() => {
+    if (account) {
+      getDatas();
+    }
+  }, [account])
+
+  const getDatas = async() => {
+    setIsLoading(true);
+
+    /*const Contract = new ethers.Contract(contractAddress, Contract.abi, provider);
+    const sellingStep = await contractAddress.sellingStep();
+
+    let wlSalePrice = await contractAddress.wlSalePrice();
+    let wlSalePriceBN = ethers.BigNumber.from(wlSalePrice._hex);
+    wlSalePrice = ethers.utils.formatEther(wlSalePriceBN);
+
+    let publicSalePrice = await contractAddress.publicSalePrice();
+    let publicSalePriceBN = ethers.BigNumber.from(publicSalePrice._hex);
+    publicSalePrice = ethers.utils.formatEther(publicSalePriceBN);
+
+    let totalSupply = await contractAddress.totalSupply();
+    totalSupply = totalSupply.toString();
+
+    setSellingStep(sellingStep);
+    setWlSalePrice(wlSalePrice);
+    setBNWlSalePrice(wlSalePriceBN);
+    setPublicSalePrice(publicSalePrice);
+    setBNPublicSalePrice(publicSalePriceBN);
+    setTotalSupply(totalSupply);*/
+
+
+    setSellingStep(0);
+    /*setWlSalePrice(wlSalePrice);
+    setBNWlSalePrice(wlSalePriceBN);
+    setPublicSalePrice(publicSalePrice);
+    setBNPublicSalePrice(publicSalePriceBN);
+    setTotalSupply(totalSupply);*/
+
+    setIsLoading(false);
+  }
+
   return (
     <Layout>
       <Flex align="center" justify="center">
-        <WhitelistSale/>
+        {isLoading ? (
+          <Spinner/>
+        ) : account ? (
+          <Flex>
+            OK
+          </Flex>
+        ) : (
+          <Text fontSize={30}>
+            Please connect your wallet
+          </Text>
+        )}
       </Flex>
     </Layout>
   )
