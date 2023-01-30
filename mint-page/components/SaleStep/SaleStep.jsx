344c9827e8fa3de06b20e148cc28a3454a06eae8
Gally <tanguy.morvant2@gmail.com>
2023-01-30 15:52:17 +0100

    Added the mint button on the sale step component

--- mint-page/components/SaleStep/SaleStep.jsx
+++ mint-page/components/SaleStep/SaleStep.jsx
@@ -29,6 +29,7 @@ const useMediaQuery = (width) => {
 
 const SaleStep = (props) => {
     const [isLoading, setIsLoading] = useState(false);
+    const [isAvaliable, setIsAvaliable] = useState(false);
     const [seconds, setSeconds] = useState(null);
     const [minutes, setMinutes] = useState(null);
     const [hours, setHours] = useState(null);
@@ -49,21 +50,27 @@ const SaleStep = (props) => {
             let unixTime = saleStartTime * 1000;
             let date_future = new Date(unixTime);
             let date_now = new Date();
+            date_now = new Date(date_now.getTime() +120*60000000);
 
-            let seconds = Math.floor((date_future - (date_now)) / 1000);
-            let minutes = Math.floor(seconds / 60);
-            let hours = Math.floor(minutes / 60);
-            let days = Math.floor(hours / 24);
-            
-            hours = hours-(days * 24);
-            minutes = minutes-(days * 24 * 60)-(hours * 60);
-            seconds = seconds-(days * 24 * 60 * 60)-(hours * 60 * 60)-(minutes * 60);
-    
-            setDays(days)
-            setHours(hours)
-            setMinutes(minutes)
-            setSeconds(seconds)
-            setIsLoading(false);
+            if (date_now > date_future) {
+                setIsLoading(false);
+                setIsAvaliable(true);
+            } else {
+                let seconds = Math.floor((date_future - (date_now)) / 1000);
+                let minutes = Math.floor(seconds / 60);
+                let hours = Math.floor(minutes / 60);
+                let days = Math.floor(hours / 24);
+                
+                hours = hours-(days * 24);
+                minutes = minutes-(days * 24 * 60)-(hours * 60);
+                seconds = seconds-(days * 24 * 60 * 60)-(hours * 60 * 60)-(minutes * 60);
+        
+                setDays(days)
+                setHours(hours)
+                setMinutes(minutes)
+                setSeconds(seconds)
+                setIsLoading(false);
+            }
         }, 1000);
     }
 
@@ -98,25 +105,36 @@ const SaleStep = (props) => {
                     ) : (<></>)}
                     <Flex direction="column" align="center" justify="center">
                         <Text fontSize={["0.75rem", "0.75rem", "1rem", "1.5rem"]}>Step {props.step}</Text>
-                        <Text fontSize={["0.75rem", "0.75rem", "1rem", "1.5rem"]}>Whitelist Sale starts in</Text>
-                        <Flex align="center" justify="center" p="2rem">
-                            <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
-                                <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{days}</Text>
-                                <Text fontStyle="italic">Days</Text>
+                        {isAvaliable ? (
+                            <Button
+                                colorScheme="cyan"
+                                fontSize={15}
+                            >
+                                Mint now !
+                            </Button>
+                        ) : (
+                        <>
+                            <Text fontSize={["0.75rem", "0.75rem", "1rem", "1.5rem"]}>Whitelist Sale starts in</Text>
+                            <Flex align="center" justify="center" p="2rem">
+                                <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
+                                    <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{days}</Text>
+                                    <Text fontStyle="italic">Days</Text>
+                                </Flex>
+                                <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
+                                    <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{hours}</Text>
+                                    <Text fontStyle="italic">Hours</Text>
+                                </Flex>
+                                <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
+                                    <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{minutes}</Text>
+                                    <Text fontStyle="italic">Mins</Text>
+                                </Flex>
+                                <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
+                                    <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{seconds}</Text>
+                                    <Text fontStyle="italic">Secs</Text>
+                                </Flex>
                             </Flex>
-                            <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
-                                <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{hours}</Text>
-                                <Text fontStyle="italic">Hours</Text>
-                            </Flex>
-                            <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
-                                <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{minutes}</Text>
-                                <Text fontStyle="italic">Mins</Text>
-                            </Flex>
-                            <Flex direction="column" justify="center" align="center" p={["1rem", "1rem", "2rem", "2rem"]}>
-                                <Text fontWeight="bold" fontSize={["1rem", "1rem", "2.5rem", "2.5rem"]}>{seconds}</Text>
-                                <Text fontStyle="italic">Secs</Text>
-                            </Flex>
-                        </Flex>
+                        </>
+                        )}
                     </Flex>
                     {props.align === "right" && !isBreakpoint ? (
                         <Flex direction="column" align="center" justify="center">
